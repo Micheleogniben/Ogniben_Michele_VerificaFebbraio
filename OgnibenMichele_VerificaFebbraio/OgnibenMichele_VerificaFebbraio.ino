b5ae2c2458756013fa506d9727abacdfead39d09
Michele <ognioscar61@gmail.com>
2019-02-16 08:56:52 +0100

    Compattazione e riordinamento del codice

--- OgnibenMichele_VerificaFebbraio/OgnibenMichele_VerificaFebbraio.ino
+++ OgnibenMichele_VerificaFebbraio/OgnibenMichele_VerificaFebbraio.ino
@@ -1,51 +1,51 @@
+                            //DICHIARAZIONE LED
 int ledRosso;
 int ledGiallo;
 int ledBianco;
 int ledVerde;
+                            //DICHIARAZIONE DURATA ACCENSIONE LED
 int durataR;
 int durataG;
 int durataB;
 int durataV;
 
 void setup() {
-  // put your setup code here, to run once:
+                            //ASSEGNAZIONE LED
   ledRosso  = 13;
   ledGiallo = 11;
   ledBianco = 7;
   ledVerde  = 5;
-  
+                            //PINMODE
   pinMode(ledRosso, OUTPUT);
   pinMode(ledGiallo, OUTPUT);
   pinMode(ledBianco, OUTPUT);
   pinMode(ledVerde, OUTPUT);
-
+                            //SERIALE
   Serial.begin(9600);
-  inputSeriale("Quanti secondi deve durare il led rosso?", durataR);
-  inputSeriale("Quanti secondi deve durare il led giallo?", durataG);
-  inputSeriale("Quanti secondi deve durare il led bianco?", durataB);
-  inputSeriale("Quanti secondi deve durare il led verde?", durataV);
+  durataR = inputSeriale("Quanti secondi deve durare il led rosso?", durataR);
+  durataG = inputSeriale("Quanti secondi deve durare il led giallo?", durataG);
+  durataB = inputSeriale("Quanti secondi deve durare il led bianco?", durataB);
+  durataV = inputSeriale("Quanti secondi deve durare il led verde?", durataV);
 }
-
 
 int inputSeriale(String frase, int var){
   Serial.println(frase);
   while (Serial.available() == 0) {};
-  var = Serial.readString().toInt() * 1000;
-  Serial.println(var + " secondi");
+  String var1 = Serial.readString();
+  Serial.println(var1);
+  var = var1.toInt() * 1000;
   return var;
 }
 
+void accendiLed(int led_on, int led_off, int ritardo){
+  digitalWrite(led_on, HIGH);
+  digitalWrite(led_off, LOW);
+  delay(ritardo);
+}
+
 void loop() {
-  digitalWrite(ledVerde, LOW);
-  digitalWrite(ledRosso, HIGH);
-  delay(durataR);
-  digitalWrite(ledRosso, LOW);
-  digitalWrite(ledGiallo, HIGH);
-  delay(durataG);
-  digitalWrite(ledGiallo, LOW);
-  digitalWrite(ledBianco, HIGH);
-  delay(durataB);
-  digitalWrite(ledBianco, LOW);
-  digitalWrite(ledVerde, HIGH);
-  delay(durataV);
+  accendiLed(ledRosso, ledVerde, durataR);
+  accendiLed(ledGiallo, ledRosso, durataG);
+  accendiLed(ledBianco, ledGiallo, durataB);
+  accendiLed(ledVerde, ledBianco, durataV);
 }
